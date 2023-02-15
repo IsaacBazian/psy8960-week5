eae3a464857767791564eec66c034f0bd2f6787b
Isaac M Bazian <bazia001@umn.edu>
2023-02-15 16:25:07 -0600

    Raw data import complete

--- R/week5.R
+++ R/week5.R
@@ -2,3 +2,14 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(lubridate)
+
+#Data Import
+Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+Anotes_tbl <- read_csv(file = "../data/Anotes.csv")
+Bdata_tbl <- read_tsv(file = "../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10)))
+Bnotes_tbl <- read_tsv(file = "../data/Bnotes.txt")
+
+
+
+
+
